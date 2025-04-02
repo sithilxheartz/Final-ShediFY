f93cd13409bae204709b9da0ae679db154b79758
Awishka Pramodya <awishkapramodya403@gmail.com>
2025-04-02 20:47:20 +0530

    created 'shift' model structure

--- lib/shift_model.dart
+++ lib/shift_model.dart
@@ -1,1 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Shift {
+  final String id;
+  final DateTime date;
+  final String pumpNumber;
+  final String pumperName;
+  final String shiftType;
+
+  Shift({
+    required this.id,
+    required this.date,
+    required this.pumpNumber,
+    required this.pumperName,
+    required this.shiftType,
+  });
+
+  // method to convert the firebase document in to a dart object
+  factory Shift.fromJson(Map<String, dynamic> json) {
+    return Shift(
+      id: json['id'] ?? '',
+      pumpNumber: json['pumpNumber'] ?? '',
+      pumperName: json['pumperName'] ?? '',
+      shiftType: json['shiftType'] ?? '',
+      date: (json['date'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
+
+  // convert the product model to a firebase document
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'pumpNumber': pumpNumber,
+      'pumperName': pumperName,
+      'shiftType': shiftType,
+      'date': date,
+    };
+  }
+}
